9779181eed59bd02bcad3697f1775ccef78481de
BrunoTaufner <brunotaufner08@gmail.com>
2021-10-31 03:32:25 -0300

    kk

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ import kernel.SeuSO;
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		List<Escalonador> escalonadores = new LinkedList<>();	
-//		escalonadores.add(SO.Escalonador.FIRST_COME_FIRST_SERVED);
-//		escalonadores.add(SO.Escalonador.SHORTEST_JOB_FIRST);
-//		escalonadores.add(SO.Escalonador.SHORTEST_REMANING_TIME_FIRST);
+		escalonadores.add(SO.Escalonador.FIRST_COME_FIRST_SERVED);
+		escalonadores.add(SO.Escalonador.SHORTEST_JOB_FIRST);
+		escalonadores.add(SO.Escalonador.SHORTEST_REMANING_TIME_FIRST);
 		escalonadores.add(SO.Escalonador.ROUND_ROBIN_QUANTUM_5);
 		String arq = "C:\\Users\\bruno\\Documents\\GitHub\\EP1 SO\\EP01-SO\\entradas\\teste16.txt";
 		for(Escalonador esc : escalonadores) {
